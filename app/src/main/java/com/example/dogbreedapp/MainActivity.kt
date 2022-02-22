9b7fcacfd63fcdd2195bf8e0627eb541ee77276b
Pramuditha Karunarathna <shemil076@gmail.com>
2022-02-23 02:43:54 +0530

    implemented to show whether the answer is correct or wrong

--- app/src/main/java/com/example/dogbreedapp/MainActivity.kt
+++ app/src/main/java/com/example/dogbreedapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dogbreedapp
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +10,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
+import java.util.Collections.shuffle
 
 class MainActivity : AppCompatActivity() {
     var Affenpinscher = mutableListOf<Int>()
@@ -20,28 +23,50 @@ class MainActivity : AppCompatActivity() {
     var Keeshond = mutableListOf<Int>()
     var Labrador = mutableListOf<Int>()
     var Rottwiler = mutableListOf<Int>()
+    var round = 1
 
     var count = 0
-    var breeds = mutableListOf(Affenpinscher, WalkerHound, Appenzeller, Chow, EskimoDog, GermanShepherd, GoldenRetriever, Keeshond,Labrador,Rottwiler)
+    var breeds = mutableListOf(
+        Affenpinscher,
+        WalkerHound,
+        Appenzeller,
+        Chow,
+        EskimoDog,
+        GermanShepherd,
+        GoldenRetriever,
+        Keeshond,
+        Labrador,
+        Rottwiler
+    )
 
-    val listOfImages = mutableListOf(listOf("img1", "img2","img3", "img4", "img5", "img6", "img7", "img8", "img9"),
-                                listOf("img10", "img11","img12","img13","img14","img15","img16","img17","img18"),
-                                listOf("img19","img20","img21","img22","img23","img24","img25","img26","img27"),
-                                listOf("img28","img29","img30","img31","img32","img33","img34","img35","img36"),
-                                listOf("img37","img38","img39","img40","img41","img42","img43","img44","img45"),
-                                listOf("img46","img47","img48","img49","img50","img51","img52","img53","img54"),
-                                listOf("img55","img56","img57","img58","img59","img60","img61","img62","img63"),
-                                listOf("img64","img65","img66","img67","img68","img69","img70","img71","img72"),
-                                listOf("img73","img74","img75","img76","img77","img78","img79","img80","img81"),
-                                listOf("img82","img83","img84","img85","img86","img87","img88","img89","img90"),
-                            )
+    val listOfImages = mutableListOf(
+        listOf("img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9"),
+        listOf("img10", "img11", "img12", "img13", "img14", "img15", "img16", "img17", "img18"),
+        listOf("img19", "img20", "img21", "img22", "img23", "img24", "img25", "img26", "img27"),
+        listOf("img28", "img29", "img30", "img31", "img32", "img33", "img34", "img35", "img36"),
+        listOf("img37", "img38", "img39", "img40", "img41", "img42", "img43", "img44", "img45"),
+        listOf("img46", "img47", "img48", "img49", "img50", "img51", "img52", "img53", "img54"),
+        listOf("img55", "img56", "img57", "img58", "img59", "img60", "img61", "img62", "img63"),
+        listOf("img64", "img65", "img66", "img67", "img68", "img69", "img70", "img71", "img72"),
+        listOf("img73", "img74", "img75", "img76", "img77", "img78", "img79", "img80", "img81"),
+        listOf("img82", "img83", "img84", "img85", "img86", "img87", "img88", "img89", "img90"),
+    )
     var breedNumber = mutableListOf<Int>()
     var gen: Random = Random()
-    var dog1 : Int? =0
-    var dog2 : Int? =0
-    var dog3 : Int? =0
+    var dog1: Int? = 0
+    var dog2: Int? = 0
+    var dog3: Int? = 0
 
+    var dogbreed1: String? = null
+    var dogbreed2: String? = null
+    var dogbreed3: String? = null
+    var displayBreedName = mutableListOf<String>()
 
+    var randomBreed: String? = null
+    var clickCount = 0
+    var goNext = false
+    var correctCount = 0
+    var incorrectCount = 0
 
 
     @SuppressLint("SetTextI18n")
@@ -53,20 +78,55 @@ class MainActivity : AppCompatActivity() {
         val image2 = findViewById<ImageView>(R.id.imageView2)
         val image3 = findViewById<ImageView>(R.id.imageView3)
         val lblMain = findViewById<TextView>(R.id.lbl_main)
+        val lblRound = findViewById<TextView>(R.id.round)
+        val result = findViewById<TextView>(R.id.result)
         val nextDogBtn = findViewById<Button>(R.id.btn_next)
 
-
+        loadDogs(lblMain, image1, image2, image3, lblRound, result)
 
         nextDogBtn.setOnClickListener {
-            loadDogs(lblMain,image1, image2, image3)
+            if (goNext) {
+                loadDogs(lblMain, image1, image2, image3, lblRound, result)
+
+            } else {
+                result.text = "⚠️Select a image before proceeding️"
+            }
+        }
+
+        image1.setOnClickListener {
+
+            dogbreed1?.let { it1 -> check(it1, result) }
+        }
+        image2.setOnClickListener {
+
+            dogbreed2?.let { it1 -> check(it1, result) }
+        }
+        image3.setOnClickListener {
+
+            dogbreed3?.let { it1 -> check(it1, result) }
         }
 
     }
 
 
-    fun loadDogs( lblMain: TextView,image1:ImageView, image2:ImageView, image3:ImageView) {
+    fun loadDogs(
+        lblMain: TextView,
+        image1: ImageView,
+        image2: ImageView,
+        image3: ImageView,
+        lblRound: TextView,
+        result: TextView
+    ) {
 
-        if ((Affenpinscher.size == 8) || (WalkerHound.size == 8) || (Appenzeller.size == 8) || (Chow.size == 8) || (EskimoDog.size == 8) || (GermanShepherd.size == 8) || (GoldenRetriever.size == 8) || (Keeshond.size == 8) || (Labrador.size == 8)||(Rottwiler.size == 8)){
+        result.setTextColor(Color.parseColor("#FF000000"))
+        result.text = ""
+        goNext = false
+        displayBreedName.clear()
+        breedNumber.clear()
+
+        if ((Affenpinscher.size == 8) || (WalkerHound.size == 8) || (Appenzeller.size == 8) || (Chow.size == 8) || (EskimoDog.size == 8) || (GermanShepherd.size == 8) || (GoldenRetriever.size == 8) || (Keeshond.size == 8) || (Labrador.size == 8) || (Rottwiler.size == 8)) {
+            round += 1
+
             breedNumber.clear()
             Affenpinscher.clear()
             WalkerHound.clear()
@@ -78,14 +138,13 @@ class MainActivity : AppCompatActivity() {
             Keeshond.clear()
             Labrador.clear()
             Rottwiler.clear()
+            lblRound.text = "Round $round"
         }
 
-        var breedName: String? = null
-        breedNumber.clear()
 
         var breed1 = 0 + gen.nextInt(8)
 
-        while(breedNumber.contains(breed1)){
+        while (breedNumber.contains(breed1)) {
             breed1 = 0 + gen.nextInt(8)
         }
         breedNumber.add(breed1)
@@ -93,7 +152,7 @@ class MainActivity : AppCompatActivity() {
 
         var breed2 = 0 + gen.nextInt(8)
 
-        while(breedNumber.contains(breed2)){
+        while (breedNumber.contains(breed2)) {
             breed2 = 0 + gen.nextInt(8)
         }
         breedNumber.add(breed2)
@@ -101,63 +160,61 @@ class MainActivity : AppCompatActivity() {
 
         var breed3 = 0 + gen.nextInt(8)
 
-        while(breedNumber.contains(breed3)){
+        while (breedNumber.contains(breed3)) {
             breed3 = 0 + gen.nextInt(8)
         }
         breedNumber.add(breed3)
 
 
 
-
         dog1 = 0 + gen.nextInt(8)
-        while(breeds[breed1].contains(dog1)){
+        while (breeds[breed1].contains(dog1)) {
             dog1 = 0 + gen.nextInt(8)
         }
         breeds[breed1].add(dog1!!)
 
         dog2 = 0 + gen.nextInt(8)
-        while(breeds[breed2].contains(dog2)){
+        while (breeds[breed2].contains(dog2)) {
             dog2 = 0 + gen.nextInt(8)
         }
         breeds[breed2].add(dog2!!)
 
         dog3 = 0 + gen.nextInt(8)
-        while(breeds[breed3].contains(dog3)){
+        while (breeds[breed3].contains(dog3)) {
             dog3 = 0 + gen.nextInt(8)
         }
         breeds[breed3].add(dog3!!)
 
-        var tempbreed1 = breeds[breed1]
-        var tempbreed2 = breeds[breed2]
-        var tempbreed3 = breeds[breed3]
 
-        var tempDog1 = "$tempbreed1  $dog1"
-        var tempDog2 = "$tempbreed2  $dog2"
-        var tempDog3 = "$tempbreed3  $dog3"
-
-        lblMain.text =""
-        lblMain.text = "$tempDog1 $tempDog2 $tempDog3"
-
-        var dogbreed1 = getBreedName(breed1)
+        dogbreed1 = getBreedName(breed1)
+        if (dogbreed1 != null) {
+            displayBreedName.add(dogbreed1!!)
+        }
 
         if (dogbreed1 != null) {
-            Log.d("test breed 1 ", dogbreed1)
-          var resource: String = listOfImages[breed1][dog1!!]
-          var resID1 = resources.getIdentifier(resource, "drawable", packageName)
-          image1.setImageResource(resID1)
+            Log.d("test breed 1 ", dogbreed1!!)
+            var resource: String = listOfImages[breed1][dog1!!]
+            var resID1 = resources.getIdentifier(resource, "drawable", packageName)
+            image1.setImageResource(resID1)
 
         }
 
-        var dogbreed2 = getBreedName(breed2)
+        dogbreed2 = getBreedName(breed2)
+        if (dogbreed2 != null) {
+            displayBreedName.add(dogbreed2!!)
+        }
 
         if (dogbreed2 != null) {
-            Log.d("test breed 2 ", dogbreed2)
+            Log.d("test breed 2 ", dogbreed2!!)
             var resource: String = listOfImages[breed2][dog2!!]
             var resID1 = resources.getIdentifier(resource, "drawable", packageName)
             image2.setImageResource(resID1)
         }
 
-        var dogbreed3 = getBreedName(breed3)
+        dogbreed3 = getBreedName(breed3)
+        if (dogbreed3 != null) {
+            displayBreedName.add(dogbreed3!!)
+        }
 
         if (dogbreed3 != null) {
 
@@ -171,7 +228,7 @@ class MainActivity : AppCompatActivity() {
             var size8 = Labrador.size
             var size9 = Keeshond.size
 
-            Log.d("test breed 3 ", dogbreed3)
+            Log.d("test breed 3 ", dogbreed3!!)
             var resource: String = listOfImages[breed3][dog3!!]
             var resID1 = resources.getIdentifier(resource, "drawable", packageName)
             image3.setImageResource(resID1)
@@ -188,13 +245,19 @@ class MainActivity : AppCompatActivity() {
             Log.d(" ", "")
         }
 
+        shuffle(displayBreedName)
+        randomBreed = displayBreedName[0]
+        lblMain.text = "Select the image of $randomBreed"
         count += 1
         Log.d("LoadDog method ran", count.toString())
+        clickCount = 0
+
+
     }
 
     fun getBreedName(breed: Int): String? {
         var breedName: String? = null
-        when (breed){
+        when (breed) {
             0 -> breedName = "Affenpinscher"
             1 -> breedName = "WalkerHound"
             2 -> breedName = "Appenzeller"
@@ -206,6 +269,27 @@ class MainActivity : AppCompatActivity() {
             8 -> breedName = "Labrador"
         }
         return breedName
+
+    }
+
+    fun check(name: String, result: TextView) {
+
+        clickCount += 1
+        if (clickCount <= 1) {
+            if (displayBreedName[0].equals(name)) {
+                result.text = "CORRECT"
+                result.setTextColor(Color.GREEN)
+                correctCount +=1
+
+            } else {
+                result.text = "WRONG"
+                result.setTextColor(Color.RED)
+                incorrectCount +=1
+            }
+        }
+        goNext = true;
+
+
     }
 
 
